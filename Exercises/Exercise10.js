999c5b5fd3dc0475d6b58b3359ea3f975f986ec5
Athira2901 <athiraprasad122@gmail.com>
2024-01-02 17:16:55 +0530

    tenth

--- Exercises/Exercise10.js
+++ Exercises/Exercise10.js
@@ -4,7 +4,7 @@ const a = {
       Name: "Virat",
       Age: 24,
       Rollno: 34,
-      marks: [{ English: 85 }, { Science: 75 }],
+      marks: [{ English: 88 }, { Science: 75 }],
     },
     MobileNos: ["1234567890", "9087654321"],
   },
@@ -18,7 +18,7 @@ b.Parent.Student.marks = [...a.Parent.Student.marks];
 b.Parent.MobileNos = [...a.Parent.MobileNos];
 b.Parent.Student.Name = "Athira Prasad";
 b.Parent.Student.Age = 23;
-b.Parent.Student.Rollno = 17;
+b.Parent.Student.Rollno = 16;
 b.Parent.Student.marks[0].English = 76;
 b.Parent.MobileNos[1] = "5372839225";
 
