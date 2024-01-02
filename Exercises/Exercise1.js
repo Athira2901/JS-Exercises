36416f4fa481172d81534ad8a5957235d7bd2887
Athira2901 <athiraprasad122@gmail.com>
2024-01-02 17:20:48 +0530

    first

--- Exercises/Exercise1.js
+++ Exercises/Exercise1.js
@@ -1,17 +1,16 @@
-let array = ['apple','banana','mango']
-for(let i=0;i<array.length;i++){
-    console.log(array[i])
-}
-let array1 = [{fruit:'apple'},{fruit:'banana'},{fruit:'mango'}]
-for(let i=0;i<array1.length;i++){
-    obj=array1[i]
-    for (let x in obj){
-        console.log(obj[x])
-    }
+let array = ["apple", "banana", "mango"];
+array.forEach((fruits) => {
+  console.log(fruits);
+});
+console.log();
 
-}
-let object = { fruit1:'apple',fruit2:'banana',fruit3:'mango'}
-for(let i in object){
-    console.log(object[i])
-}
+let array1 = [{ fruit: "apple" }, { fruit: "banana" }, { fruit: "mango" }];
+array1.map((fruits) => {
+  console.log(fruits.fruit);
+});
+console.log();
 
+let object = { fruit1: "apple", fruit2: "banana", fruit3: "mango" };
+for (let i in object) {
+  console.log(object[i]);
+}
