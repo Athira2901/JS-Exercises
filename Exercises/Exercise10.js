8734999d29d02288c866e8171b6c20c0f9ee444e
Athira2901 <athiraprasad122@gmail.com>
2024-01-03 10:27:22 +0530

    tenth

--- Exercises/Exercise10.js
+++ Exercises/Exercise10.js
@@ -1,3 +1,4 @@
+
 const a = {
   Parent: {
     Student: {
@@ -10,17 +11,17 @@ const a = {
   },
 };
 const b = { ...a };
-
+console.log(a)
+console.log(b)
 b.Parent = Object.assign({}, a.Parent);
 
 b.Parent.Student = Object.assign({}, a.Parent.Student);
-b.Parent.Student.marks = [...a.Parent.Student.marks];
-b.Parent.MobileNos = [...a.Parent.MobileNos];
+b.Parent.Student.marks = a.Parent.Student.marks.map((mark)=>Object.assign({},mark))
 b.Parent.Student.Name = "Athira Prasad";
 b.Parent.Student.Age = 23;
 b.Parent.Student.Rollno = 16;
-b.Parent.Student.marks[0].English = 76;
-b.Parent.MobileNos[1] = "5372839225";
+b.Parent.Student.marks[1].Science = 76;
+b.Parent.MobileNos[1] = "5372839223";
 
 console.log(JSON.stringify(a));
 console.log(JSON.stringify(b));
